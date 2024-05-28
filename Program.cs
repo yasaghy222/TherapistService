50c1b2dfbf88d54a9e844d05a06dac155103cf15
yasagy222 <yasaghy222@gmail.com>
2024-05-28 19:02:56 +0330

    fix issue

--- Program.cs
+++ Program.cs
@@ -5,38 +5,49 @@ using TherapistService.Data;
 using Microsoft.EntityFrameworkCore;
 
 
-// Add services to the container.
-WebApplication.CreateBuilder(args).Services.RegisterMapsterConfiguration();
-WebApplication.CreateBuilder(args).Services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
-
-string? defName = WebApplication.CreateBuilder(args).Configuration["Db:Name"];
-string? defHost = WebApplication.CreateBuilder(args).Configuration["Db:Host"];
-string? defPass = WebApplication.CreateBuilder(args).Configuration["Db:Pass"];
-string? dbHost = Environment.GetEnvironmentVariable("DB_HOST") ?? defHost;
-string? dbName = Environment.GetEnvironmentVariable("DB_NAME") ?? defName;
-string? dbPass = Environment.GetEnvironmentVariable("DB_SA_PASSWORD") ?? defPass;
-
-string? connectionString = $"Server={dbHost}; Persist Security Info=False; TrustServerCertificate=true; User ID=sa;Password={dbPass};Initial Catalog={dbName};";
-WebApplication.CreateBuilder(args).Services.AddDbContext<TherapistServiceContext>(options => options.UseSqlServer(connectionString));
-
-WebApplication.CreateBuilder(args).Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-WebApplication.CreateBuilder(args).Services.AddEndpointsApiExplorer();
-WebApplication.CreateBuilder(args).Services.AddSwaggerGen();
-
-var app = WebApplication.CreateBuilder(args).Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public static void Main(string[] args)
+    {
+        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.RegisterMapsterConfiguration();
+        builder.Services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+
+        string? defName = builder.Configuration["Db:Name"];
+        string? defHost = builder.Configuration["Db:Host"];
+        string? defPass = builder.Configuration["Db:Pass"];
+        string? dbHost = Environment.GetEnvironmentVariable("DB_HOST") ?? defHost;
+        string? dbName = Environment.GetEnvironmentVariable("DB_NAME") ?? defName;
+        string? dbPass = Environment.GetEnvironmentVariable("DB_SA_PASSWORD") ?? defPass;
+
+        string? connectionString = $"Server={dbHost}; Persist Security Info=False; TrustServerCertificate=true; User ID=sa;Password={dbPass};Initial Catalog={dbName};";
+        builder.Services.AddDbContext<TherapistServiceContext>(options => options.UseSqlServer(connectionString));
+
+        builder.Services.AddControllers();
+
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        WebApplication app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
